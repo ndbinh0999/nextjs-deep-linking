19aea7e57335b705ac4407e172d1a8a550e5f3d4
binh.nguyen2 <binh.nguyen2@bnksolution.com>
2024-04-05 08:58:22 +0700

    update-main

--- pages/[lang]/sharing/[type]/[id].tsx
+++ pages/[lang]/sharing/[type]/[id].tsx
@@ -16,165 +16,199 @@ export enum caseHandle {
   activity = "activity",
 }
 
-const DeepLinkPage = () => {
+const DeepLinkPage = ({
+  title,
+  imageUrl,
+}: {
+  title: string;
+  imageUrl: string;
+}) => {
   const router = useRouter();
   const { lang, type: prefix, id: encryptId } = router.query;
   const [data, setData] = useState<any>();
-
-  const userAgent = typeof navigator !== "undefined" && navigator.userAgent;
-  const redirectUrl = getAppStoreUrl(userAgent);
   const params = decrypt(
     encryptId as string,
     "e1342b084a6585295c7ade62c211f850"
   );
 
-  console.table([params, encryptId]);
+  console.log("params", params);
 
-  function decrypt(valueStringHex: string, keyStringHex: string) {
-    if (!valueStringHex || valueStringHex.length === 0) {
-      console.error("Invalid valueStringHex:", valueStringHex);
-      return "";
-    }
-    const value = CryptoJS.enc.Hex.parse(valueStringHex);
-    const key = CryptoJS.enc.Utf8.parse(keyStringHex);
-    const ivvar = CryptoJS.enc.Utf8.parse("4a6585295c7ade62");
-    const decryptedStringHex = CryptoJS.AES.decrypt(
-      { ciphertext: value } as any,
-      key,
-      {
-        iv: ivvar,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-      }
-    );
-    return decryptedStringHex.toString(CryptoJS.enc.Utf8);
-  }
+  useEffect(() => {
+    if (!prefix) return;
 
-  async function redirectBasedOnDevice() {
-    let apiUrl;
-    if (
-      [caseHandle.trending, caseHandle.learning_resource].includes(
-        prefix as caseHandle
-      )
-    ) {
-      apiUrl = `${process.env.NEXT_PUBLIC_API_CMS_STRAPI}/api/${
-        convertPrefix[prefix as string]
-      }?filters[hyab_content_id]=${params}&populate[0]=banner`;
-    }
-    if (prefix === caseHandle.activity) {
-      apiUrl = `${process.env.NEXT_PUBLIC_API_HOST_HYAB_ACTIVITY}/api/Activities/${params}`;
-    }
+    const fetchData = async () => {
+      try {
+        let apiUrl;
+        if (
+          [caseHandle.trending, caseHandle.learning_resource].includes(
+            prefix as caseHandle
+          )
+        ) {
+          apiUrl = `${process.env.NEXT_PUBLIC_API_CMS_STRAPI}/api/${
+            convertPrefix[prefix as string]
+          }?filters[hyab_content_id]=${params}&populate[0]=banner`;
+        }
+        if (prefix === caseHandle.activity) {
+          apiUrl = `${process.env.NEXT_PUBLIC_API_HOST_HYAB_ACTIVITY}/api/Activities/${params}`;
+        }
 
-    if (!apiUrl) return;
-
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        if (response) {
+        const response = await axios.get(apiUrl);
+        if (response && response.data) {
           prefix === caseHandle.activity
             ? setData(response)
             : setData(response.data);
         }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-
-    if (redirectUrl) {
-      try {
-        const isFirefox = userAgent.indexOf("FxiOS") !== -1;
-        const isChrome = userAgent.indexOf("CriOS") !== -1;
-        const isEdge = userAgent.indexOf("EdgiOS") !== -1;
-        const isOpera = userAgent.indexOf("OPT") !== -1;
-        const isSafari = !isFirefox && !isChrome && !isEdge && !isOpera;
-        const isFacebookMessenger =
-          /FBAN|FBAV|FBMD|FBSN|FBSS|FBCR|FBID|FBLC|FBOP|FBRV|FB_IAB|FBBV|FBDM|FBFV/i.test(
-            userAgent
-          );
-
-        if (
-          /huawei/i.test(userAgent) ||
-          /android/i.test(userAgent) ||
-          (/iPad|iPhone/.test(userAgent) && !isSafari) ||
-          isFacebookMessenger
-        ) {
-          window.location.replace(
-            `hyab://youth/sharing/${prefix}/${encryptId}`
-          );
-        }
-
-        setTimeout(() => {
-          window.location.replace(redirectUrl);
-        }, 2000);
       } catch (error) {
-        console.error("Error during URL redirection:", error);
+        console.error("Error fetching data:", error);
       }
-    } else {
-      alert(
-        "Sorry, unable to open on desktop. Please open this link on your mobile app."
-      );
-    }
-  }
+    };
 
-  const getTitle = (): string => {
-    if (prefix === caseHandle.trending) {
-      return data?.data[0]?.attributes?.title;
-    }
-    if (prefix === caseHandle.learning_resource) {
-      return data?.data[0]?.attributes?.TITLE;
-    }
-    if (prefix === caseHandle.activity) {
-      return data?.data?.nameEN;
-    }
-    return "";
-  };
+    fetchData();
 
-  const getImage = (): string => {
-    if (prefix === caseHandle.trending) {
-      return `${process.env.NEXT_PUBLIC_API_CMS_STRAPI}/${data?.data[0]?.attributes.banner.data.attributes.url}`;
-    }
-    if (prefix === caseHandle.learning_resource) {
-      return data?.data[0]?.attributes?.THUMB;
-    }
-    if (
-      prefix === caseHandle.activity &&
-      data?.data.activityPhotos.length !== 0
-    ) {
-      return `${process.env.NEXT_PUBLIC_API_HOST_HYAB_ACTIVITY}/api/Photos/${data?.data.activityPhotos[0].id}`;
-    }
-    return "";
-  };
+    const redirectBasedOnDevice = () => {
+      const userAgent = typeof navigator !== "undefined" && navigator.userAgent;
+      const redirectUrl = getAppStoreUrl(userAgent);
+      if (!redirectUrl) {
+        alert(
+          "Sorry, unable to open on desktop. Please open this link on your mobile app."
+        );
+        return;
+      }
 
-  function getAppStoreUrl(userAgent: string | undefined) {
-    if (/huawei/i.test(userAgent)) {
-      return "https://appgallery.huawei.com/#/app/C110441477?appId=C110441477";
-    }
-    if (/android/i.test(userAgent)) {
-      return "https://play.google.com/store/apps/details?id=hk.gov.youthapp";
-    }
-    if (/iPad|iPhone|iPod/.test(userAgent)) {
-      return "https://apps.apple.com/app/HKYouth+/6478738189";
-    }
-    return null;
-  }
+      if (redirectUrl) {
+        try {
+          const isFirefox = userAgent.indexOf("FxiOS") !== -1;
+          const isChrome = userAgent.indexOf("CriOS") !== -1;
+          const isEdge = userAgent.indexOf("EdgiOS") !== -1;
+          const isOpera = userAgent.indexOf("OPT") !== -1;
+          const isSafari = !isFirefox && !isChrome && !isEdge && !isOpera;
+          const isFacebookMessenger =
+            /FBAN|FBAV|FBMD|FBSN|FBSS|FBCR|FBID|FBLC|FBOP|FBRV|FB_IAB|FBBV|FBDM|FBFV/i.test(
+              userAgent
+            );
 
-  useEffect(() => {
-    prefix && redirectBasedOnDevice();
+          if (
+            /huawei/i.test(userAgent) ||
+            /android/i.test(userAgent) ||
+            /iPad|iPhone/.test(userAgent) ||
+            isFacebookMessenger
+          ) {
+            window.location.replace(
+              `hyab://youth/sharing/${prefix}/${encryptId}`
+            );
+          }
+          setTimeout(() => {
+            window.location.replace(redirectUrl);
+          }, 2000);
+        } catch (error) {
+          console.error("Error during URL redirection:", error);
+        }
+      } else {
+        alert(
+          "Sorry, unable to open on desktop. Please open this link on your mobile app."
+        );
+      }
+    };
+
+    redirectBasedOnDevice();
   }, [prefix]);
-
-  console.log("getTitle:", getTitle());
-  console.log("getImage:", getImage());
-
   return (
     <div>
       <Head>
-        {/* <title>{getTitle()}</title> */}
-        <meta property="og:title" content={getTitle()} />
-        <meta property="og:image" content={getImage()} />
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta property="og:image" content={imageUrl} />
       </Head>
-      {getTitle()}
+      <div>{title}</div>
+      <img src={imageUrl} alt="Image" />
     </div>
   );
 };
 
+export async function getServerSideProps(context: any) {
+  const { params } = context;
+  const { type, id: encryptId } = params;
+  const prefix = type as string;
+  const paramsDecrypted = decrypt(
+    encryptId,
+    "e1342b084a6585295c7ade62c211f850"
+  );
+
+  let apiUrl;
+  if (
+    [caseHandle.trending, caseHandle.learning_resource].includes(
+      prefix as caseHandle
+    )
+  ) {
+    apiUrl = `${process.env.NEXT_PUBLIC_API_CMS_STRAPI}/api/${
+      convertPrefix[prefix as string]
+    }?filters[hyab_content_id]=${paramsDecrypted}&populate[0]=banner`;
+  }
+  if (prefix === caseHandle.activity) {
+    apiUrl = `${process.env.NEXT_PUBLIC_API_HOST_HYAB_ACTIVITY}/api/Activities/${paramsDecrypted}`;
+  }
+
+  try {
+    const response = await axios.get(apiUrl);
+    let title = "";
+    let imageUrl = "";
+    if (response && response.data) {
+      const responseData = response.data;
+      if (prefix === caseHandle.trending) {
+        title = responseData?.data[0]?.attributes?.title || "";
+        imageUrl = `${process.env.NEXT_PUBLIC_API_CMS_STRAPI}/${responseData?.data[0]?.attributes.banner.data.attributes.url}`;
+      } else if (prefix === caseHandle.learning_resource) {
+        title = responseData?.data[0]?.attributes?.TITLE || "";
+        imageUrl = responseData?.data[0]?.attributes?.THUMB || "";
+      } else if (prefix === caseHandle.activity) {
+        title = responseData?.data?.nameEN || "";
+        if (responseData?.data.activityPhotos.length !== 0) {
+          imageUrl = `${process.env.NEXT_PUBLIC_API_HOST_HYAB_ACTIVITY}/api/Photos/${responseData?.data.activityPhotos[0].id}`;
+        }
+      }
+    }
+    return {
+      props: { title, imageUrl },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: { title: "", imageUrl: "" },
+    };
+  }
+}
+
 export default DeepLinkPage;
+
+function decrypt(valueStringHex: string, keyStringHex: string) {
+  if (!valueStringHex || valueStringHex.length === 0) {
+    console.error("Invalid valueStringHex:", valueStringHex);
+    return "";
+  }
+  const value = CryptoJS.enc.Hex.parse(valueStringHex);
+  const key = CryptoJS.enc.Utf8.parse(keyStringHex);
+  const ivvar = CryptoJS.enc.Utf8.parse("4a6585295c7ade62");
+  const decryptedStringHex = CryptoJS.AES.decrypt(
+    { ciphertext: value } as any,
+    key,
+    {
+      iv: ivvar,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    }
+  );
+  return decryptedStringHex.toString(CryptoJS.enc.Utf8);
+}
+
+function getAppStoreUrl(userAgent: any) {
+  if (/huawei/i.test(userAgent)) {
+    return "https://appgallery.huawei.com/#/app/C110441477?appId=C110441477";
+  }
+  if (/android/i.test(userAgent)) {
+    return "https://play.google.com/store/apps/details?id=hk.gov.youthapp";
+  }
+  if (/iPad|iPhone|iPod/.test(userAgent)) {
+    return "https://apps.apple.com/app/HKYouth+/6478738189";
+  }
+  return null;
+}
